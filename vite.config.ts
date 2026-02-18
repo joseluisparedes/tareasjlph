8f78fe75fb1659744f4631782de3601425fb46be
José Luis <joseluisparedes@example.com>
2026-02-18 10:15:13 -0500

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import autoprefixer from 'autoprefixer';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/tareasjlph/',
     server: {
       port: 3000,
       host: '0.0.0.0',
